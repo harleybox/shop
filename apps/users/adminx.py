87dce6ee3687e392e038cfa15857fa478a1c9904
harley <hharley@gmail.com>
2018-11-30 14:41:30 +0800

    1st

---
+++ apps/users/adminx.py
@@ -0,0 +1,26 @@
+__author__ = 'Harley'
+__date__ = '2018-11-29 21:40'
+
+import xadmin
+from xadmin import views
+from .models import VerifyCode
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = "慕学生鲜后台"
+    site_footer = "mxshop"
+    # menu_style = "accordion"
+
+
+class VerifyCodeAdmin(object):
+    list_display = ['code', 'mobile', "add_time"]
+
+
+xadmin.site.register(VerifyCode, VerifyCodeAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
--- shop/settings.py
+++ shop/settings.py
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'DjangoUeditor',
+    'goods.apps.GoodsConfig',
+    'user_operation.apps.UserOperationConfig',
+    'trade.apps.TradeConfig',
+    'crispy_forms',
+    'django.contrib.admin',
+    'xadmin',
 ]
 
 MIDDLEWARE = [
@@ -108,15 +115,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
