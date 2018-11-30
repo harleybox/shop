d59ceeaccec47ca99d6bb39ad12cf6c799057b4b
harley <hharley@gmail.com>
2018-11-30 22:28:52 +0800

    import data

--- shop/urls.py
+++ shop/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+# from shop.settings import MEDIA_ROOT
+# from django.views.static import serve
 
 import xadmin
 
+
 urlpatterns = [
      path('xadmin/', xadmin.site.urls),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
